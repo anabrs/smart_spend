abd5509b0ba436e7a4393fa26cbd70a83a19949d
Ana Barrios <abarrios.chavez@gmail.com>
2025-04-21 13:24:16 -0600

    Devise authentication

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org"
 
 gem "rails", "~> 8.0.2"
 
+gem "devise", "~> 4.9"
 gem "importmap-rails"
 gem "jbuilder"
 gem "propshaft"
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :entries
+
+  root to: "home#index"
 end
